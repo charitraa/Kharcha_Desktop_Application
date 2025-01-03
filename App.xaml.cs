c90afd5e30c86732ce3ec857615397bc1f00bc2c
charitraa <stharabi9862187405@gmail.com>
2025-01-03 23:19:52 +0545

    added coded

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Expenses_tracker
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
