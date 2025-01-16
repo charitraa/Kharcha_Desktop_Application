1a7c7609f3e92b0cd7db5acaf1564651d9f3a633
charitraa <stharabi9862187405@gmail.com>
2025-01-16 15:43:15 +0545

    minor chnages

---
+++ wwwroot/scripts.js
@@ -0,0 +1,6 @@
+function downloadFile(fileName, base64Data) {
+    const link = document.createElement("a");
+    link.href = "data:text/csv;base64," + base64Data;
+    link.download = fileName;
+    link.click();
+}
